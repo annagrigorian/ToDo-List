f9354327fbe9585c0a67e1171b434f3ee5cea206
Anna Grigorian <anna.grigorian11@gmail.com>
2019-04-10 20:02:06 +0400

    Add Grouping

---
+++ ToDo_List_Class_Library/ToDo_List_Class_Library/ToDoItem.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_List_Class_Library
+{
+    public class ToDoItem
+    {
+        #region Properties
+
+        public string Category { get; set; }
+
+        public string Name { get; set; }
+
+        public int ID { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public ToDoItem(string category, string name)
+        {
+            Category = category;
+            Name = name;
+        }
+
+        public ToDoItem() : this("No Category", "UnNamed")
+        { }
+
+        #endregion
+
+        #region Required Overrides
+
+        public override string ToString()
+        {
+            return $"Catgory is {Category} \n  Name  is {Name} \n\t IsCompleted {((IsCompleted) ? "Yes" : "No")}";
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            else if (this.GetType() != obj.GetType()) return false;
+            else return false;
+        }
+
+        public static bool operator ==(ToDoItem todo1, ToDoItem todo2)
+        {
+            return ((todo1.Name == todo2.Name)
+                && (todo1.Category == todo2.Category)
+                && (todo1.ID == todo2.ID)
+                && (todo1.IsCompleted == todo2.IsCompleted));
+        }
+
+        public static bool operator !=(ToDoItem todo1, ToDoItem todo2)
+        {
+            return !(todo1 == todo2);
+        }
+
+        #endregion
+
+    }
+}
---
+++ ToDo_List_Class_Library/ToDo_List_Class_Library/ToDoList.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_List_Class_Library
+{
+    public class ToDoList : IEnumerable<ToDoItem>
+    {
+        private List<ToDoItem> toDos = new List<ToDoItem>();
+
+        #region Constructors
+
+        public ToDoList() { }
+
+        #endregion
+
+        #region Properties
+
+        public int Count => toDos.Count();
+
+        #endregion
+
+        #region IEnumerable Interface Implementation
+
+        public IEnumerator<ToDoItem> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return toDos[i];
+            };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        #endregion
+
+        #region Helper Methods     
+
+        public void Add(ToDoItem todo)
+        {
+            toDos.Add(todo);
+        }
+
+        public void Completed(ToDoItem todo)
+        {
+            todo.IsCompleted = true;
+        }
+
+        public void NotCompleted(ToDoItem todo)
+        {
+            todo.IsCompleted = false;
+        }
+
+        public IEnumerable<ToDoItem> Remove()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                if (toDos[i].IsCompleted)
+                {
+                    toDos.RemoveAt(i);
+                }
+            }
+            return toDos;
+        }
+
+        public IEnumerable<ToDoItem> ShowAll()
+        {
+            return this.Select(t => t);
+        }
+
+        public IEnumerable<ToDoItem> ShowCompleted()
+        {
+            return this.Where(t => t.IsCompleted == true);
+        }
+
+        public IEnumerable<ToDoItem> ShowNotCompleted()
+        {
+            return this.Where(t => t.IsCompleted == false);
+        }
+
+        public IEnumerable<IGrouping<string,ToDoItem>> GroupBy()
+        {
+            var grouped = this.GroupBy(i => i.Category);
+            return grouped;
+        }
+
+        #endregion
+
+        #region Required Overrides
+
+        public override string ToString()
+        {
+            return GetEnumerator().ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj)) return true;
+            else if (this.GetType() != obj.GetType()) return false;
+            else return false;
+        }
+
+        public static bool operator ==(ToDoList todos1, ToDoList todos2)
+        {
+            return todos1 == todos2;
+        }
+
+        public static bool operator !=(ToDoList todos1, ToDoList todos2)
+        {
+            return !(todos1 == todos2);
+        }
+
+
+        #endregion
+
+    }
+}
